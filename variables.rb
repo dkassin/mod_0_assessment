f4495e461e808947ecadded49fc899a57d5b916b
David Kassin <david.j.kassin@gmail.com>
2021-09-24 14:30:02 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+movie = "Batman Begins"
+is_entertaining = true
+movie = "The Dark Knight"
+num_4 = 4
+num_6 = 6
+product = num_4 * num_6
+favorite_foods = ["Sushi", "Steak", "Fish"]
+favorite_foods.shift
+p movie.length
