e4bfdd3117cca2ccd2baf9f04bdd1eb6572358b8
David Kassin <david.j.kassin@gmail.com>
2021-09-24 14:42:53 -0600

    Add model_a challenge

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur = {
+  "name": "Chuck",
+  "type": "Velociraptor",
+  "number_of_legs": 2,
+  "number_of_arms": 2,
+  "can_fly": false,
+  "vegitarian": false,
+  "favorite_foods": ["People", "Dinosaurs", "Chickens"],
+  "locations_lived": ["Mongolia", "China", "Jurassic Park"]
+  }
