fce760c13d60b336bacae9a943493b2deb638cd4
David Kassin <david.j.kassin@gmail.com>
2021-09-24 14:49:12 -0600

    Add model_b challenge

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+  "name": "Jim",
+  "gender": "Male",
+  "years_experience": 4,
+  "different_positions": 2,
+  "self_taught": false,
+  "wears_glasses": true,
+  "languages_known": ["Python","Ruby","C++"],
+  "favorite_foods": ["Chicken","Sushi","Steak"]
+}
